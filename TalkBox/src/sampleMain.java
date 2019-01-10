5019b5aa5f25d9475909d9e13aae707d3201e625
Jessie Leung <leungj73@gmail.com>
2019-01-10 10:25:01 -0500

    Test commit from Jessie.

--- TalkBox/src/sampleMain.java
+++ TalkBox/src/sampleMain.java
@@ -4,7 +4,7 @@ public class sampleMain {
 	public static void main(String[] args) {
 		System.out.println("Fun with Git");
 		System.out.println("Hello from collaborator");
-
+		System.out.println("Hello from 2nd collaborator");
 	}
 
 }
