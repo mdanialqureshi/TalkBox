b977c9954e7d64bd7496a1eb48a48aac51b38be4
xmbhasin <xmbhasin@gmail.com>
2019-01-14 11:45:35 -0500

    Initial TalkBox GUI

---
+++ TalkBox/src/TalkBoxGUI/TalkBoxMain.java
@@ -0,0 +1,47 @@
+package TalkBoxGUI;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import java.awt.FlowLayout;
+import javax.swing.JButton;
+
+public class TalkBoxMain extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					TalkBoxMain frame = new TalkBoxMain();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public TalkBoxMain() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
+		
+		JButton btnTalk = new JButton("Talk");
+		contentPane.add(btnTalk);
+	}
+
+}
