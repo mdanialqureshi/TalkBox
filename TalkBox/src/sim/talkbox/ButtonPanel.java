7d4e4f44b8478efe43fbdaa1592c8cac40635c7a
Danial Qureshi <danialqureshi.m@gmail.com>
2019-01-21 14:05:15 -0500

    Fixing master branch

--- TalkBox/src/sim/talkbox/ButtonPanel.java
+++ TalkBox/src/sim/talkbox/ButtonPanel.java
@@ -97,7 +97,7 @@ public class ButtonPanel extends JPanel {
 	public void playSound(String soundName) {
 
 		try {
-			File file = new File(this.getClass().getResource("/TalkBoxData/audio/" + soundName).getFile()); // gets the file from its
+			File file = new File(this.getClass().getResource("/" + soundName).getFile()); // gets the file from its
 																							// package using file name
 			Clip clip = AudioSystem.getClip();
 			clip.open(AudioSystem.getAudioInputStream(file));
