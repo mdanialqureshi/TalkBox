dcf88ac75ae38c20e003056f09d768aaaf1e60c3
xmbhasin <xmbhasin@gmail.com>
2019-01-21 12:28:40 -0500

    Adds stubbed TalkBoxSerializer class that implements TalkBoxConfiguration

---
+++ TalkBox/src/config/talkbox/TalkBoxSerializer.java
@@ -0,0 +1,34 @@
+package config.talkbox;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class TalkBoxSerializer implements TalkBoxConfiguration {
+
+	@Override
+	public int getNumberOfAudioButtons() {
+		return 6;
+	}
+
+	@Override
+	public int getNumberOfAudioSets() {
+		return 1;
+	}
+
+	@Override
+	public int getTotalNumberOfButtons() {
+		// 6 audio playback buttons + 1 audio set cycle button + 1 profile load button
+		return 8;
+	}
+
+	@Override
+	public Path getRelativePathToAudioFiles() {
+		return Paths.get("TalkBoxData/audio");
+	}
+
+	@Override
+	public String[][] getAudioFileNames() {
+		return null;
+	}
+
+}
