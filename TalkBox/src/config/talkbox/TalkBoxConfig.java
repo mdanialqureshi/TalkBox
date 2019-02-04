445d17eb479fa79136075ed81eff96b61b6de206
Danial Qureshi <danialqureshi.m@gmail.com>
2019-02-04 17:49:23 -0500

    Testing Documents update

--- TalkBox/src/config/talkbox/TalkBoxConfig.java
+++ TalkBox/src/config/talkbox/TalkBoxConfig.java
@@ -20,7 +20,7 @@ public class TalkBoxConfig extends JFrame {
 	private int width = 1280;
 	private int height = 720;
 	
-	public static int numAudButtons = 20;
+	public static int numAudButtons = 10;
 	public static int numAudSets = 1;
 	public static int numSwapButtons = 2;
 	public static Path path = null;
@@ -60,7 +60,7 @@ public class TalkBoxConfig extends JFrame {
 		setupFrame();
 		
 	}
-
+	
 	
 	private void setupFrame() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
