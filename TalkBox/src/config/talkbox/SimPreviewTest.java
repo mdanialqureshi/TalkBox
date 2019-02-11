7bba5908ea22b2506fdcc6c9e754c470391fe045
xmbhasin <xmbhasin@gmail.com>
2019-02-10 19:32:33 -0500

    Add initial tests for sim preview

---
+++ TalkBox/src/config/talkbox/SimPreviewTest.java
@@ -0,0 +1,33 @@
+package config.talkbox;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class SimPreviewTest {
+
+	private SimPreview sp;
+
+	@BeforeEach
+	void setUp() throws Exception {
+		sp = new SimPreview();
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	@Test
+	void test() throws InterruptedException {
+		assertEquals(sp.buttons.size(), 20);
+		sp.updateButtons(5);
+		Thread.sleep(1000);
+		assertEquals(sp.buttons.size(), 5);
+		sp.updateButtons(25);
+		Thread.sleep(1000);
+		assertEquals(sp.buttons.size(), 25);
+	}
+
+}
