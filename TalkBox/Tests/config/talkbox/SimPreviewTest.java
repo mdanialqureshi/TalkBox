a0496c25498028bb2327de7900325374f0eb9af4
xmbhasin <xmbhasin@gmail.com>
2019-02-10 21:29:59 -0500

    Add test for playing back audio in sim preview

--- TalkBox/Tests/config/talkbox/SimPreviewTest.java
+++ TalkBox/Tests/config/talkbox/SimPreviewTest.java
@@ -1,21 +1,23 @@
 package config.talkbox;
 
-
 import static org.junit.Assert.assertEquals;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import config.talkbox.*;
-
 class SimPreviewTest {
 
 	private SimPreview sp;
+	private TalkBoxConfig tbc;
 
 	@BeforeEach
 	void setUp() throws Exception {
 		sp = new SimPreview();
+		tbc = new TalkBoxConfig();
 	}
 
 	@AfterEach
@@ -34,5 +36,15 @@ class SimPreviewTest {
 		sp.updateButtons(25);
 		assertEquals(25, sp.buttons.size());
 		assertEquals(25, sp.buttonsPanel.getComponentCount());
+	}
+
+	@Test
+	void testPlayingSound() throws InterruptedException {
+		final ByteArrayOutputStream myErr = new ByteArrayOutputStream();
+		System.setErr(new PrintStream(myErr));
+		tbc.audFileNames[0][0] = "audio/hello.wav";
+		sp.buttons.get(0).doClick();
+		
+		assertEquals("", myErr.toString());
 	}
 }
