d17b2ca79afab6b883d3eb84a445991aa3fd8366
xmbhasin <xmbhasin@gmail.com>
2019-02-22 12:58:23 -0500

    Add serializability to ProfileList and implement conversion to 2d String array

--- TalkBox/src/config/talkbox/Profile.java
+++ TalkBox/src/config/talkbox/Profile.java
@@ -1,23 +1,21 @@
 package config.talkbox;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 
-public class Profile {
-	
+public class Profile implements Serializable {
+
+	private static final long serialVersionUID = 5064938306897688957L;
 	
 	protected String profileName;
 	protected ArrayList<String> audioFileNames;
-	
-	/**
-	 * Default no parameter constructor
-	 */
+
 	public Profile(String profileName) {
 		this.profileName = profileName;
 		audioFileNames = new ArrayList<String>();
-		
 	}
-	
-	
-	
 
+	public ArrayList<String> getAudioFileNames() {
+		return audioFileNames;
+	}
 }
--- TalkBox/src/config/talkbox/ProfileList.java
+++ TalkBox/src/config/talkbox/ProfileList.java
@@ -1,25 +1,23 @@
 package config.talkbox;
 
+import java.io.Serializable;
 import java.util.LinkedList;
 
-public class ProfileList<Profile> extends LinkedList<Profile>{
+public class ProfileList extends LinkedList<Profile> implements Serializable {
 
-	private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = -1677747622460303106L;
 
-	
 	public ProfileList() {
 		super();
 	}
-	
-	
-	protected String[][] toArrayMatrix(){
-		
-		
-		return null;
+
+	protected String[][] toArrayMatrix() {
+
+		String[][] audioFileNames = new String[TalkBoxConfig.numAudSets][TalkBoxConfig.numAudButtons];
+		for (int i = 0; i < TalkBoxConfig.numAudSets; ++i) {
+			audioFileNames[i] = this.get(i).getAudioFileNames().toArray(new String[TalkBoxConfig.numAudButtons]);
+		}
+		return audioFileNames;
 	}
-	
-	
-	
-	
-	
+
 }
