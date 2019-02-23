5e9ff1bacc305e12370d8f565ae326ede083f723
Jessie Leung <leungj73@gmail.com>
2019-02-23 12:30:46 -0500

    TalkBoxSim tests.

--- TalkBox/Tests/sim/talkbox/TalkBoxSimTest.java
+++ TalkBox/Tests/sim/talkbox/TalkBoxSimTest.java
@@ -2,18 +2,38 @@ package sim.talkbox;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import config.talkbox.Recorder;
+import config.talkbox.SimRecorderSplit;
+import config.talkbox.TalkBoxConfig;
+
 class TalkBoxSimTest {
 
+	private TalkBoxConfig tbc;
+	private Recorder recObj;
+	private ButtonPanel btnPnl;
+	private TalkBoxSim tbs; 
+	
 	@BeforeEach
 	void setUp() throws Exception {
+		tbc = new TalkBoxConfig();
+		recObj = ((SimRecorderSplit) tbc.controlsProfileSplit.getLeftComponent()).recorder;
+	}
+	
+	@AfterEach
+	void tearDown() throws Exception{
 	}
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void testUpdatingButtons() throws InterruptedException {
+		recObj.txtNumberOfButtons.setText("30");
+		recObj.updateNumberOfButtons.doClick();
+		Thread.sleep(1000);
+		recObj.launchSimulator.doClick();
+		assertEquals(30,TalkBoxConfig.numAudButtons);
 	}
 
 }
