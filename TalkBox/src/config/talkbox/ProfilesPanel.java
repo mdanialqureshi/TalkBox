3fd09fc33424ea9c7fadd79265580df8f0aa9170
Danial Qureshi <danialqureshi.m@gmail.com>
2019-03-18 12:55:33 -0400

    Added logger gui to config app

--- TalkBox/src/config/talkbox/ProfilesPanel.java
+++ TalkBox/src/config/talkbox/ProfilesPanel.java
@@ -6,7 +6,16 @@ import java.awt.Font;
 import java.awt.Insets;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 
 import javax.swing.DefaultListModel;
 import javax.swing.JButton;
@@ -15,7 +24,9 @@ import javax.swing.JLabel;
 import javax.swing.JList;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
 import javax.swing.JTextField;
+import javax.swing.JTextPane;
 import javax.swing.ListSelectionModel;
 import javax.swing.ScrollPaneConstants;
 import javax.swing.SwingConstants;
@@ -28,11 +39,14 @@ public class ProfilesPanel extends JPanel {
 	private final JFileChooser fc;
 	DefaultListModel<String> profilesListModel;
 	JList<String> profilesJList;
-	private int loadedProfile  = 0;
+	private int loadedProfile = 0;
 	private int selectedProfile = 0;
 	JButton loadProf;
 	JButton newProf;
 	JButton delProf;
+	JTextArea textArea;
+	File[] logFiles;
+	int currentLogFile = 0;
 
 	public ProfilesPanel() {
 		fc = new JFileChooser();
@@ -58,7 +72,7 @@ public class ProfilesPanel extends JPanel {
 
 		profilesJList = new JList<String>(profilesListModel);
 
-		profilesJList.setBounds(175, 51, 242, 368);
+		profilesJList.setBounds(6, 34, 242, 368);
 		profilesJList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
 		add(profilesJList);
 
@@ -77,12 +91,12 @@ public class ProfilesPanel extends JPanel {
 		JScrollPane profiles = new JScrollPane(profilesJList);
 		profiles.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
 		profiles.setViewportBorder(new LineBorder(Color.GRAY));
-		profiles.setBounds(25, 93, 260, 375);
+		profiles.setBounds(10, 93, 260, 236);
 		add(profiles);
 
 		// Profiles search
 		JLabel lblProfSearch = new JLabel("Search for a profile:");
-		lblProfSearch.setBounds(24, 68, 261, 21);
+		lblProfSearch.setBounds(9, 68, 261, 21);
 		add(lblProfSearch);
 
 		JTextField textField = new JTextField();
@@ -92,7 +106,7 @@ public class ProfilesPanel extends JPanel {
 		// Buttons
 		loadProf = new JButton("Load Profile");
 		loadProf.setMargin(new Insets(0, 0, 0, 0));
-		loadProf.setBounds(30, 470, 110, 30);
+		loadProf.setBounds(20, 329, 110, 30);
 		loadProf.setHorizontalAlignment(SwingConstants.CENTER);
 		loadProf.setVerticalAlignment(SwingConstants.CENTER);
 		add(loadProf);
@@ -104,7 +118,7 @@ public class ProfilesPanel extends JPanel {
 
 		newProf = new JButton("New Profile");
 		newProf.setMargin(new Insets(0, 0, 0, 0));
-		newProf.setBounds(164, 470, 110, 30);
+		newProf.setBounds(142, 329, 110, 30);
 		newProf.setHorizontalAlignment(SwingConstants.CENTER);
 		newProf.setVerticalAlignment(SwingConstants.CENTER);
 		add(newProf);
@@ -121,10 +135,70 @@ public class ProfilesPanel extends JPanel {
 				deleteProfile();
 			}
 		});
-		delProf.setBounds(30, 505, 110, 30);
+		delProf.setBounds(78, 364, 110, 30);
 		delProf.setHorizontalAlignment(SwingConstants.CENTER);
 		delProf.setVerticalAlignment(SwingConstants.CENTER);
 		add(delProf);
+
+		textArea = new JTextArea();
+		textArea.setEditable(false);
+		textArea.setBounds(25, 417, 245, 214);
+		textArea.setLineWrap(true);
+		readLogs();
+		add(textArea);
+
+		JLabel label = new JLabel("Logs: ");
+		label.setBounds(21, 394, 261, 21);
+		add(label);
+
+		JButton btnPreviousLog = new JButton("Previous Log");
+		btnPreviousLog.setBounds(23, 638, 117, 29);
+		btnPreviousLog.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				readCurrentLog("prev");
+			}
+		});
+		add(btnPreviousLog);
+
+		JButton btnNextLog = new JButton("Next Log");
+		btnNextLog.setBounds(153, 638, 117, 29);
+		btnNextLog.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				readCurrentLog("next");
+			}
+		});
+		add(btnNextLog);
+
+	}
+
+	protected void readCurrentLog(String target) {
+		if (logFiles.length > 0) {
+			if (target.equals("prev") && currentLogFile < logFiles.length - 1) {
+				currentLogFile++;
+			} else if (target.equals("next") && currentLogFile > 0) {
+				currentLogFile--;
+			}
+		}
+		try {
+			BufferedReader input = new BufferedReader(
+					new InputStreamReader(new FileInputStream(logFiles[currentLogFile])));
+			textArea.read(input, "Reading Log File");
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	protected void readLogs() {
+		File simLogs = new File(TalkBoxConfig.talkBoxDataPath, "sim-logs");
+		if (simLogs.isDirectory())
+			;
+		logFiles = simLogs.listFiles();
+		Arrays.sort(logFiles, Collections.reverseOrder());
+		readCurrentLog("current");
+
 	}
 
 	protected void loadSelectedProfile() {
@@ -134,7 +208,7 @@ public class ProfilesPanel extends JPanel {
 		for (int i = 0; i < TalkBoxConfig.numAudButtons; ++i) {
 			SimRecorderSplit.simPreview.buttons.get(i).setAudioFile(audioFiles.get(i));
 		}
-		SimPreview.profileNumber.setText("  Profile " + (TalkBoxConfig.profilesList.getCurrentProfile()+1));
+		SimPreview.profileNumber.setText("  Profile " + (TalkBoxConfig.profilesList.getCurrentProfile() + 1));
 	}
 
 	protected void deleteProfile() {
