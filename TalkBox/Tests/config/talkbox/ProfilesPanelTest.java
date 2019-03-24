006c0974451c262f2f2268632f06ef80be757df1
Danial Qureshi <danialqureshi.m@gmail.com>
2019-03-24 17:31:09 -0400

    updated testing docs and fixed gradle test error from profilespaneltest

--- TalkBox/Tests/config/talkbox/ProfilesPanelTest.java
+++ TalkBox/Tests/config/talkbox/ProfilesPanelTest.java
@@ -1,6 +1,6 @@
 package config.talkbox;
 
-import static org.junit.Assert.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
